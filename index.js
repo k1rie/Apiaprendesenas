6d805a96db7bc5d77f9bcb2df5748e47af1d87da
k1rie <d628587@gmail.com>
2023-05-04 21:42:56 -0600

    0.01

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {Video,run} from "./mongoConfig/mongoConfig.js"
+import {findCourse, findAllCoursesComunity, createVideo, findCourseComunity} from "./mongoConfig/mongoConfig.js"
 import cors from "cors"
 import Nodemailer from "nodemailer"
 const app = express()
@@ -29,17 +29,39 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.get("/curso/:name",async (req,res)=>{
-    let videos = await run(Video,req.params.name)
+app.get("/curso/:id",async (req,res)=>{
+    console.log(req.params.id)
+    let videos = await findCourse(req.params.id)
     res.send(videos)
     console.log(videos)
 } )
+
+app.get("/cursocomunity/:id",async (req,res)=>{
+    console.log(req.params.id)
+    let videos = await findCourseComunity(req.params.id)
+    res.send(videos)
+    console.log(videos)
+} )
+
+app.get("/cursoscomunity/",async (req,res)=>{
+    let videos = await findAllCoursesComunity()
+    res.send(videos)
+    console.log(videos)
+})
 
 
 app.post("/sendsugerencia",async (req,res)=>{
     sendsugerencia(req.body.name,req.body.sugerencia)
     console.log(req.body)
     res.send("se envio la sugerencia")
+})
+
+
+app.post("/createvideo",async (req,res)=>{
+    const body = req.body
+
+await createVideo(body.url,body.urlportada,body.name,body.aprendizajes,body.description,body.language,body.isaprove)
+    res.send("enviado")
 })
 
 app.get("/hola",(req,res)=>{
--- mongoConfig/mongoConfig.js
+++ mongoConfig/mongoConfig.js
@@ -7,40 +7,91 @@ const {Schema} = mongoose
 
 const urlConnect = "mongodb+srv://lenguajesenasfree:dwY6tV5qCVgrDKGn@cursosdb.now8332.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect(urlConnect,{useNewUrlParser: true, useUnifiedTopology: true})
 
-const client = new MongoClient(urlConnect, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+const cursoSchema = new Schema({
+    url: String,
+    name: String,
+    aprendizajes: Array,
+    description: String,
+    language: String
+})
 
-async function run(model,name){
+
+const cursoComunitySchema = new Schema({
+    url: String,
+    urlportada: String,
+    name: String,
+    aprendizajes: Array,
+    description: String,
+    language: String,
+    isaprove: Boolean
+})
+
+const model = mongoose.model("cursos",cursoSchema)
+
+const modelComunity = mongoose.model("cursos-comunity",cursoComunitySchema)
+
+
+async function findCourse(id){
     try{
-        await client.connect();
+        await mongoose.connect(urlConnect,{useNewUrlParser: true, useUnifiedTopology: true});
         console.log("conectado")
 
-            const resultados = await model.find({name : name} )
+            const resultados = await model.find({_id : id} )
             return resultados
 
-    }finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+    }catch (e) {
+        console.log(e)
     }
+}
+
+async function findCourseComunity(id){
+    try{
+        await mongoose.connect(urlConnect,{useNewUrlParser: true, useUnifiedTopology: true});
+        console.log("conectado")
+
+        const resultados = await modelComunity.find({_id : id} )
+        return resultados
+
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+async function findAllCoursesComunity(name){
+    try {
+        await mongoose.connect(urlConnect,{useNewUrlParser: true, useUnifiedTopology: true});
+        console.log("conectado")
+        const resultados = await modelComunity.find({isaprove: true} )
+        return resultados
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+async function createVideo(url,urlportada,name,aprendizajes,description,language,isaprove){
+try {
+    await mongoose.connect(urlConnect,{useNewUrlParser: true, useUnifiedTopology: true});
+    const newvideo = await new modelComunity({
+        url: url,
+        urlportada: urlportada,
+        name: name,
+        aprendizajes: aprendizajes,
+        description: description,
+        language: language,
+        isaprove: isaprove
+    })
+    newvideo.save()
+
+}catch (e) {
+    console.log(e)
+}
 }
 
 
 
-const videoUrlSchema = new Schema({
-    url: String,
-    description: String,
-    name: String
-})
-
-const Video = mongoose.model("videos",videoUrlSchema)
 
 
 
-export {Video,run}
+
+export {findCourse,findAllCoursesComunity,createVideo,findCourseComunity}
